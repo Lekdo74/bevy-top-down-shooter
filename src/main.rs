7f9ea6236109c0092bcc0522be89b927b74c481d
Lekdo74 <alexandre.giniaux@etu.univ-smb.fr>
2024-06-15 16:22:08 +0200

    spawn player

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,22 @@ use bevy::prelude::*;
 use bevy::render::settings::{Backends, RenderCreation, WgpuSettings};
 use bevy::render::RenderPlugin;
 
+pub fn close_on_esc(
+    mut commands: Commands<'_, '_>,
+    focused_windows: Query<'_, '_, (Entity, &Window)>,
+    input: Res<'_, ButtonInput<KeyCode>>,
+) {
+    for (window, focus) in focused_windows.iter() {
+        if !focus.focused {
+            continue;
+        }
+
+        if input.just_pressed(KeyCode::Escape) {
+            commands.entity(window).despawn();
+        }
+    }
+}
+
 const WW: f32 = 1200.0;
 const WH: f32 = 900.0;
 const BG_COLOR: (u8, u8, u8) = (25, 20, 43);
@@ -31,27 +47,33 @@ fn main() {
         .insert_resource(ClearColor(Color::srgb_u8(
             BG_COLOR.0, BG_COLOR.1, BG_COLOR.2,
         )))
-        .add_systems(Startup, setup)
+        .add_systems(Startup, (setup_camera, spawn_player))
         .add_systems(Update, close_on_esc)
         .run();
 }
 
-fn setup(mut commands: Commands) {
+fn setup_camera(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
 }
 
-pub fn close_on_esc(
-    mut commands: Commands<'_, '_>,
-    focused_windows: Query<'_, '_, (Entity, &Window)>,
-    input: Res<'_, ButtonInput<KeyCode>>
+fn spawn_player(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut texture_atlas_layouts: ResMut<Assets<TextureAtlasLayout>>,
 ) {
-    for (window, focus) in focused_windows.iter() {
-        if !focus.focused {
-            continue;
-        }
+    let texture = asset_server.load("assets.png");
+    let layout = TextureAtlasLayout::from_grid(UVec2::splat(24), 7, 1, None, None);
+    let texture_atlas_layout = texture_atlas_layouts.add(layout);
 
-        if input.just_pressed(KeyCode::Escape) {
-            commands.entity(window).despawn();
-        }
-    }
+    commands.spawn((
+        SpriteBundle {
+            transform: Transform::from_scale(Vec3::splat(6.0)),
+            texture,
+            ..default()
+        },
+        TextureAtlas {
+            layout: texture_atlas_layout,
+            index: 0,
+        },
+    ));
 }
